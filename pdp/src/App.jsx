6ee1c16e09deda3e4311a048ad13ebc77b4dd355
Ryan Van Valkenburg <ryanv@trustnota.com>
2021-11-13 17:19:55 -0800

    Establish API server and architecture pattern

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { Suspense }from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
+const Header = React.lazy(() =>  import("home/Header"));
+import Footer from "home/Footer";
+import SafeComponent from "./SafeComponent";
 
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: pdp</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Suspense fallback={ <div>Loading...</div> }>
+      <SafeComponent>
+        <Header />
+      </SafeComponent>
+    </Suspense>
+    <div className="my-10">
+      PDP Page Content
+    </div>
+    <Footer />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
