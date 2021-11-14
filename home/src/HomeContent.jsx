f19d0a1d0e6399479a8b10d64139b587019e9474
Ryan Van Valkenburg <ryanv@trustnota.com>
2021-11-13 17:43:45 -0800

    Fix return for images

--- home/src/HomeContent.jsx
+++ home/src/HomeContent.jsx
@@ -2,17 +2,24 @@ import React, { useState, useEffect } from 'react';
 import { getProducts, currency } from './products';
 
 export default function HomeContent() {
-  const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    getProducts().then(setProducts);
-  }, []);
+    useEffect(() => {
+        getProducts().then(setProducts);
+    }, []);
 
     return <div className="grid grid-cols-4 gap-5">
-        { products.map((product) => {
-            <div key={ product.id }>
-                <img src={ product.image } alt={ product.name} />
+         {products.map((product) => (
+            <div key={product.id}>
+                <img src={ product.image } alt={ product.name } />
+                <div className="flex">
+                    <div className="flex-grow font-bold">
+                        <a>{ product.name }</a>
+                     </div>
+                     <div className="flex-end">{ currency.format(product.price) }</div>
+                </div>
+                <div className=" text-sm mt-4">{ product.description }</div>
             </div>
-        })}
+        ))}
     </div>
 }
